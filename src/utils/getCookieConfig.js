e097b8d72b88f926aa91d72aa22b879b3ea7893e
Artem Synytsin <sh1nodartem@gmail.com>
2023-11-28 12:39:35 +0200

    add domain env variable

--- src/utils/getCookieConfig.js
+++ src/utils/getCookieConfig.js
@@ -1,12 +1,10 @@
 export const getCookieConfig = () => {
-  const isLocalEnvironment = process.env.NODE_ENV === "development";
-
   return {
     maxAge: 183 * 24 * 60 * 60 * 1000, // 6 months
     httpOnly: true, // HTTP only flag
     secure: true, // Set to true if your application is using HTTPS
     sameSite: "None", // Allow cross-site requests
     partitioned: true,
-    domain: isLocalEnvironment ? "localhost" : ".pages.dev",
+    domain: process.env.COOKIE_DOMAIN,
   };
 };
