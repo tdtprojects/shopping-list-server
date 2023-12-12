1ed5e37ddc931d88542fe92c1eb8e03c21de066c
Artem Synytsin <sh1nodartem@gmail.com>
2023-12-12 13:07:43 +0200

    update scheduler to 3 months

--- src/scheduler.js
+++ src/scheduler.js
@@ -3,15 +3,12 @@ import schedule from "node-schedule";
 import ShoppingList from "./models/ShoppingList.js";
 
 schedule.scheduleJob("0 0 * * *", async () => {
-  // const threeMonthsAgo = new Date();
-  // threeMonthsAgo.setMonth(threeMonthsAgo.getMonth() - 3);
-
-  const twoDaysAgo = new Date();
-  twoDaysAgo.setDate(twoDaysAgo.getDate() - 2);
+  const threeMonthsAgo = new Date();
+  threeMonthsAgo.setMonth(threeMonthsAgo.getMonth() - 3);
 
   try {
-    // const result = await ShoppingList.deleteMany({ updatedAt: { $lt: threeMonthsAgo } });
-    const result = await ShoppingList.deleteMany({ updatedAt: { $lt: twoDaysAgo } });
+    const result = await ShoppingList.deleteMany({ updatedAt: { $lt: threeMonthsAgo } });
+
     console.log(`${result.deletedCount} shopping lists deleted.`);
   } catch (error) {
     console.error("Error when deleting lists:", error);
